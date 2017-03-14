d6fe771da80616dfc63c8efbf46f7dcaf9bac061
Joel Day <git@dayjo.me>
2017-03-14 12:07:50 +0000

    add everything in, write tests, include pipelines

---
+++ example.js
@@ -0,0 +1,16 @@
+var FeedAnalyser = require('./index.js');
+
+var feed = ['Hello my name is joel and I work for simon and ben', 'I work with arran', 'ben works in chichester'];
+var keywords = ['joel','arran','simon','ben'];
+
+
+
+// Run the analysis
+FeedAnalyser.analyse(feed, keywords)
+  .then((analysis) => {
+      console.log({ status: 1, analysis: analysis });
+  })
+  .catch((err) => {
+      console.log({ status: 0, statusMessage: err });
+  });
+
---
+++ test/feed-analyser.spec.js
@@ -0,0 +1,69 @@
+var chai = require('chai');
+const expect = chai.expect;
+var assert = require('assert');
+var FeedAnalyser = require('../index.js');
+
+const demoFeed = ['Hello my name is joel and I work for simon and ben', 'I work with arran', 'ben works in chichester'];
+const demoKeywords = ['joel','arran','simon','ben'];
+
+describe('FeedAnalyser', function(){
+
+    describe('cleanArray', function(){
+        it('should return a cleaned feed', function(){
+            let cleanedArray = FeedAnalyser.cleanArray(['ØnE','tWœ','ThRêe']);
+
+            expect(cleanedArray).to.deep.equal(['one','two','three']);
+        })
+    });
+
+    describe('cleanString', function(){
+        it('should return a cleaned string', function(){
+            let cleanedFeed = FeedAnalyser.cleanString('ØnE tWœ ThRêe');
+
+            expect(cleanedFeed).to.equal('one two three');
+        })
+    });
+
+    describe('cleanKeywords', function(){
+        it('should return a cleaned array of keywords', function(){
+            let cleanedKeywords = FeedAnalyser.cleanKeywords(['ØnE','tWœ','ThRêe']);
+
+            expect(cleanedKeywords).to.deep.equal(['one','two','three']);
+        })
+    });
+
+    describe('cleanFeed', function(){
+        it('should return a cleaned feed', function(){
+            let cleanedFeed = FeedAnalyser.cleanFeed(['ØnE','tWœ','ThRêe']);
+
+            expect(cleanedFeed).to.deep.equal(['one','two','three']);
+        })
+    });
+
+
+    describe('analyse', function(){
+        it ('should return a promise', function(){
+            expect(FeedAnalyser.analyse(['one'],['two'])).to.be.a('promise');
+        });
+
+        it ('should resolve a valid analysis', function() {
+            return FeedAnalyser.analyse(['one'],['two']).then(function(f){
+                expect(f).to.be.an('object');
+            });
+        });
+
+    });
+
+    describe('qualityScore', function(){
+        it('should return a floating point number', function(){
+            expect(FeedAnalyser.qualityScore()).to.equal('0.000');
+        });
+
+        it('should return a valid quality score', function(){
+            return FeedAnalyser.analyse(demoFeed,demoKeywords).then(function(){
+                expect(FeedAnalyser.qualityScore()).to.equal('1.500');
+            });
+        });
+    });
+
+});
